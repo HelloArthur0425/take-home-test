52ee7cfb96cc4ea48de604766809f7c45ce9af11
Arthur Cheng <63392897+arthurCheng12@users.noreply.github.com>
2021-09-15 17:21:17 +0800

    basic layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col } from 'react-bootstrap';
+import Header from './components/Header/Header.js';
 
 function App() {
   return (
     <div className="App">
-      <div>HelloWorld</div>
+      <Header />
+      <Container>
+        <Row>
+          <Col xs={2} md={2} lg={2}>
+            <div>
+              Left
+            </div>
+          </Col>
+          <Col xs={8} md={8} lg={8}>
+            <div></div>
+          </Col>
+          <Col xs={2} md={2} lg={2}>
+            <div>
+              Chat room
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import react from 'react';
+import './Header.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Container, Row, Col } from 'react-bootstrap';
+
+const Header = () => {
+    return <Navbar bg="dark" variant="dark">
+        <Container>
+            <Navbar.Brand href="#home">
+                <Row className="logo-container">
+                    <Col><div className="logo">T</div></Col>
+                    <Col><div className="logo-name">TakeHome</div></Col>
+                </Row>
+            </Navbar.Brand>
+        </Container>
+    </Navbar> 
+}
+
+export default Header;
